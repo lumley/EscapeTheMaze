5d6927f6b1887cb334fb6efaf30faea3d92c4217
lumley <lumley256@gmail.com>
2015-10-06 23:22:10 +0200

    Added debug information on runtime

---
+++ Assets/Scripts/Debug/StaticFollower.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StaticFollower : MonoBehaviour {
+
+    public GameObject gameObjectToFollow;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Debug.isDebugBuild)
+        {
+            this.transform.position = gameObjectToFollow.transform.position;
+        }
+	}
+}
