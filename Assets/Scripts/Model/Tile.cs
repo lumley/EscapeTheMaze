3039f8dfb04f47b9fea73eb7eeab35d2448510fd
lumley <lumley256@gmail.com>
2015-10-09 20:50:53 +0200

    Minor structural preparation for TileAttributes

--- Assets/Scripts/Model/Tile.cs
+++ Assets/Scripts/Model/Tile.cs
@@ -5,6 +5,7 @@ namespace Model
     public class Tile
     {
         private Dictionary<Direction, Tile> neighbourMap = new Dictionary<Direction,Tile>(4);
+        private Dictionary<int, TileComponent> attributeMap;
 
         public void SetNeighbour(Tile neighbour, Direction directionFromMe) // TODO: Unit test this!
         {
@@ -24,6 +25,15 @@ namespace Model
             Tile neighbour;
             this.neighbourMap.TryGetValue(directionFromMe, out neighbour);
             return neighbour;
+        }
+
+        public bool HasAttribute<T>(int attributeType)
+        {
+            if (this.attributeMap != null)
+            {
+                return this.attributeMap.ContainsKey(attributeType);
+            }
+            return false;
         }
 
     }
---
+++ Assets/Scripts/Model/TileComponents.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Model
+{
+    public interface TileComponent
+    {
+
+    }
+
+    namespace TileAttribute
+    {
+        public class SpawningPoint : TileComponent
+        {
+
+        }
+
+        public class EndingPoint : TileComponent
+        {
+
+        }
+    }
+}
