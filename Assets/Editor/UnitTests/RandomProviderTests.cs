4b568559d84e756d87f0c2f2f4bd7c1f2e46fe06
lumley <lumley256@gmail.com>
2015-10-11 21:05:46 +0200

    Added unit testing framework and first unit test

---
+++ Assets/Editor/UnitTests/RandomProviderTests.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using NUnit.Framework;
+
+public class RandomProviderTests {
+
+    private RandomProvider randomProvider;
+    private int seedBefore;
+
+    [SetUp]
+    public void SetUp()
+    {
+        this.randomProvider = new RandomProvider();
+        this.randomProvider.seed = 0;
+
+        seedBefore = UnityEngine.Random.seed;
+        UnityEngine.Random.seed = this.randomProvider.seed;
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        UnityEngine.Random.seed = seedBefore;
+    }
+
+    [Test]
+    public void GetRandomElementExcludingShouldNotReturnAnElementExcluded()
+    {
+        int excludedValue = 0;
+        int[] elements = { excludedValue, excludedValue+1 };
+        int[] excluded = { excludedValue };
+
+        for(int i=0; i< 1000; ++i)
+        {
+            int element = this.randomProvider.GetRandomElementExcluding(elements, excluded);
+            Assert.AreNotEqual(excludedValue, element);
+        }
+    }
+
+}
