fd6ac7ff83cf68ee6efe092cad6093b48460161c
Giancarlo Dessena <giancarlo.dessena@googlemail.com>
2015-11-01 13:31:45 +0100

    added a compass for better orientation

---
+++ Assets/Scripts/HUD/Compass.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Compass : MonoBehaviour {
+
+	public void SetRotation(float rotation){
+		transform.Rotate (0, 0, rotation);
+	}
+}
--- Assets/Scripts/OrientationController.cs
+++ Assets/Scripts/OrientationController.cs
@@ -5,6 +5,7 @@ public class OrientationController : MonoBehaviour {
 
 	public void SetRotation(float rotation){
 		transform.Rotate (0, rotation, 0);
+		GameObject.Find ("Compass").SendMessage ("SetRotation", rotation);
 	}
 
 }
